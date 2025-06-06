d317c4fe531601ce83622685371d611cd359b498
Minhcadarnian <markbuicadarnian@gmail.com>
2025-06-06 21:37:08 +0700

    [33-rest] cover assigment

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -37,3 +37,28 @@ TEST_CASE("Scalar multiplication", "[Matrix]") {
     REQUIRE( S(1,1) == 4 ); REQUIRE( S(1,2) == 6 );
     REQUIRE( S(2,1) == 8 ); REQUIRE( S(2,2) == 10 );
 }
+
+TEST_CASE("Matrix assignment copies values", "[Matrix]") {
+    Matrix A(2,2), B(2,2);
+    A(1,1)=1; A(1,2)=2; A(2,1)=3; A(2,2)=4;
+    B(1,1)=5; B(1,2)=6; B(2,1)=7; B(2,2)=8;
+
+    A = B;
+    REQUIRE(A.rows() == 2);
+    REQUIRE(A.cols() == 2);
+    for (std::size_t i=1;i<=2;++i)
+        for (std::size_t j=1;j<=2;++j)
+            REQUIRE(A(i,j) == Approx(B(i,j)));
+}
+
+TEST_CASE("Matrix assignment reallocates for size change", "[Matrix]") {
+    Matrix A(2,3);
+    Matrix B(3,1);
+    B(1,1)=1; B(2,1)=2; B(3,1)=3;
+
+    A = B;
+    REQUIRE(A.rows() == 3);
+    REQUIRE(A.cols() == 1);
+    for (std::size_t i=1;i<=3;++i)
+        REQUIRE(A(i,1) == Approx(B(i,1)));
+}
